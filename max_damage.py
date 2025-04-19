2a468c609b2e51b3f3da789e64007327eccb7f18
Annhilati <antbe.artis@web.de>
2025-04-19 12:10:38 +0200

    Sync

---
+++ max_damage.py
@@ -0,0 +1,4 @@
+chestplate_max_damage = 745
+
+max_damage = [0.6875 * chestplate_max_damage, chestplate_max_damage, 0.9375 * chestplate_max_damage, 0.8152 * chestplate_max_damage]
+print(max_damage)
