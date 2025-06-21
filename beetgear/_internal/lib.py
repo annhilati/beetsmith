7373ca7197cf6be9c32d00a72e6cd49516f9066d
Annhilati <antbe.artis@web.de>
2025-06-21 22:28:13 +0200

    Update lib.py

--- beetgear/_internal/lib.py
+++ beetgear/_internal/lib.py
@@ -2,7 +2,7 @@ import re
 import json
 import string
 from pydantic import BaseModel
-from typing import Any, Type
+from typing import Any, Type, Union, List, Dict
 
 resourceLocationPattern = r"^[a-z0-9](?:[a-z0-9._-]*[a-z0-9])?:[a-z0-9](?:[a-z0-9._-]*[a-z0-9])?(?:\/[a-z0-9](?:[a-z0-9._-]*[a-z0-9])?)*$" # currently: never leading special symbols
 
@@ -17,7 +17,7 @@ def resourceLocation(str: str):
     
     return str
 
-def textComponent(obj: str | dict | list[Any]) -> str | dict | list[Any]:
+def textComponent(obj: Any) -> str | dict | list[Any]:
     if isinstance(obj, str):
         try:
             work = json.loads(obj)
@@ -37,3 +37,34 @@ def ersetzen_template(obj: Any, mapping: dict[str, Any]) -> Any:
         return {k: ersetzen_template(v, mapping) for k, v in obj.items()}
     else:
         return obj
+
+def get_structure(obj: Any) -> type:
+
+    if isinstance(obj, str):
+        working = str
+    elif isinstance(obj, list):
+        working = List
+        types: set = set()
+        if len(obj) == 0:
+            working = List[None]
+        else:
+            for e in obj:
+                structure = get_structure(e)
+                types.add(structure)
+            working = List[Union[tuple(types)]]
+    elif isinstance(obj, dict):
+        working = Dict
+        types: set = set()
+        for key, value in obj.items():
+            value_structure = get_structure(value)
+            if get_structure(key) is not str:
+                raise NotImplementedError()
+            types.add(value_structure)
+        working = Dict[str, Union[tuple(types)]]
+    else:
+        working = type(obj)
+
+    return working
+
+
+print(get_structure({"test": ["liste", ["liste"]]}))
