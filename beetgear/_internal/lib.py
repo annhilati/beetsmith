d731c1a7c1bc0eff09bf06175e506e042d8b65fe
Annhilati <antbe.artis@web.de>
2025-06-22 14:33:54 +0200

    Remade textComponent function

--- beetgear/_internal/lib.py
+++ beetgear/_internal/lib.py
@@ -17,54 +17,56 @@ def resourceLocation(str: str):
     
     return str
 
-def textComponent(obj: Any) -> str | dict | list[Any]:
-    if isinstance(obj, str):
-        try:
-            work = json.loads(obj)
-        except Exception as e:
-            work = obj
+def textComponent(obj: Any) -> list[list[dict]]:
+    """Ensures that a text representing object is always a complete and correct formatted multiline text component
+
+    #### Additional use:
+        - [0]: if only one line is allowed in the field
+    """
+    newLines = []
+
+    if type(obj) == list:                       # obj ist Line oder Multiline
+        if list in [type(e) for e in obj]:      # obj ist Multiline
+            for line in obj:
+                if type(line) == str:                   # line ist str
+                    newLines.append([{"text": line}])
+                elif type(line) == dict:                # line ist dict
+                    newLines.append([line])
+                elif type(line) == list:                # line ist list
+                    newLine = []
+                    for part in line:
+                        if type(part) == str:               # part ist str
+                            newLine.append({"text": part})
+                        elif type(part) == dict:            # part ist dict
+                            newLine.append(part)
+                        else:
+                            raise ValueError("Every part in a line in the object has to be a dictionary or a string")
+                    newLines.append(newLine)
+                else:                                   # line ist nicht str, dict oder list
+                    raise ValueError("Every line in the object has to be a list, a dictionary or a string")
+        else:                                   # obj ist Line
+            newLine = []
+            for part in obj:
+                if type(part) == str:                   # part ist str
+                    newLine.append({"text": part})
+                elif type(part) == dict:                # part ist dict
+                    newLine.append(part)
+            newLines.append(newLine)
+    elif type(obj) == dict:
+        newLines.append([obj])
+    elif type(obj) == str:
+        newLines.append([{"text": obj}])
     else:
-        work = obj
-    
-    return work
+        ValueError("Object has to be a list, a dictionary or a string")
 
-def ersetzen_template(obj: Any, mapping: dict[str, Any]) -> Any:
-    if isinstance(obj, str):
-        return string.Template(obj).safe_substitute(mapping)
-    elif isinstance(obj, list):
-        return [ersetzen_template(e, mapping) for e in obj]
-    elif isinstance(obj, dict):
-        return {k: ersetzen_template(v, mapping) for k, v in obj.items()}
-    else:
-        return obj
+    return newLines
 
-def get_structure(obj: Any) -> type:
-
-    if isinstance(obj, str):
-        working = str
-    elif isinstance(obj, list):
-        working = List
-        types: set = set()
-        if len(obj) == 0:
-            working = List[None]
-        else:
-            for e in obj:
-                structure = get_structure(e)
-                types.add(structure)
-            working = List[Union[tuple(types)]]
-    elif isinstance(obj, dict):
-        working = Dict
-        types: set = set()
-        for key, value in obj.items():
-            value_structure = get_structure(value)
-            if get_structure(key) is not str:
-                raise NotImplementedError()
-            types.add(value_structure)
-        working = Dict[str, Union[tuple(types)]]
-    else:
-        working = type(obj)
-
-    return working
-
-
-print(get_structure({"test": ["liste", ["liste"]]}))
+# def ersetzen_template(obj: Any, mapping: dict[str, Any]) -> Any:
+#     if isinstance(obj, str):
+#         return string.Template(obj).safe_substitute(mapping)
+#     elif isinstance(obj, list):
+#         return [ersetzen_template(e, mapping) for e in obj]
+#     elif isinstance(obj, dict):
+#         return {k: ersetzen_template(v, mapping) for k, v in obj.items()}
+#     else:
+#         return obj
