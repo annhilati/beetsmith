12ed5b4f62ae816029a2bfee7edd52e2480571d6
Annhilati <antbe.artis@web.de>
2025-07-20 17:02:58 +0200

    Sync

--- beetsmith/library/utils.py
+++ beetsmith/library/utils.py
@@ -1,8 +1,3 @@
-import beet
-from typing import Literal
-from beetsmith.core.classes import CustomItem
-from beetsmith.core.models import *
-
 def armor_durability(*, helmet: int = None, chestplate: int = None, leggings: int = None, boots: int = None) -> tuple[int, int, int, int]:
     
     if len([arg for arg in [helmet, chestplate, leggings, boots] if type(arg) != None]) in [0, 2, 3]:
@@ -25,34 +20,3 @@ def armor_durability(*, helmet: int = None, chestplate: int = None, leggings: in
                     durabilities[slot] = round(durabilities[other_slot] * factors[slot][other_slot])
     
     return durabilities
-
-def shaped_recipe(result: CustomItem,
-                  items: tuple[tuple[str, str, str], tuple[str, str, str], tuple[str, str, str]],
-                  category: Literal["building", "redstone", "misc", "equipment"] = "misc")-> beet.Recipe:
-        alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i"]
-        pattern = []
-        register = {}
-        for row in items:
-            pattern_row = ""
-            for item in row:
-                if item is None:
-                    pattern_row += " "
-                elif item in register:
-                    pattern_row += register[item]
-                else:
-                    pattern_row += alphabet[len(register)]
-                    register[item] = alphabet[len(register)]
-            pattern.append(pattern_row)
-
-        json = {
-            "type": "minecraft:crafting_shaped",
-            "category": category,
-            "pattern": pattern,
-            "key": {key: item for item, key in register.items()},
-            "result": {
-                "id": result.item,
-                "components": result._components_data
-            }
-        }
-
-        return beet.Recipe(json)
