d57ea385c783ced44e84e14baf4addcfb3aae02e
Annhilati <antbe.artis@web.de>
2025-09-23 12:12:05 +0200

    Update components.py

--- beetsmith/v2/components.py
+++ beetsmith/v2/components.py
@@ -1,21 +1,28 @@
+# https://minecraft.wiki/w/Data_component_format#List_of_components
+# https://misode.github.io/changelog?tags=component
 import requests
-import json
-from dataclasses import dataclass, asdict
+from dataclasses import dataclass, fields
 from typing import Any
 
 class RemovedComponentState(object):
-    def __str__() -> str:
+    def __str__(self) -> str:
         return "REMOVED"
 
 REMOVED = RemovedComponentState()
 "Constant denoting that an item component is removed"
+ValidInComponentValue = list["ValidInComponentValue"] | dict[str, "ValidInComponentValue"] | int | float | str
+ValidComponentValue = None | RemovedComponentState | ValidInComponentValue
 
 @dataclass
 class ItemComponents():
     """Class representing a Minecraft item's components.
+
+    Supports
+    ---------
+    - `str(·)`
+    - `·[...]`
+    - `·[...] = ...`
     """
-    # https://minecraft.wiki/w/Data_component_format#List_of_components
-    # https://misode.github.io/changelog?tags=component
     attribute_modifiers:            list[dict]          = None
     block_attacks:                  dict                = None
     break_sound:                    str                 = None
@@ -50,10 +57,11 @@ class ItemComponents():
     use_cooldown:                   dict                = None
     use_remainder:                  dict                = None
     weapon:                         dict                = None
-    _modded_components:             dict                = None
+
+    _other_components:              dict[str, ValidComponentValue] = None
 
     def __str__(self) -> str:
-        return json.dumps(asdict(self), indent=4)
+        return str(self.asDict())
 
     @classmethod
     def fromVanillaItem(cls, id: str):
@@ -70,37 +78,92 @@ class ItemComponents():
         return cls.fromDict(data[query])
 
     @classmethod
-    def fromDict(cls, data: dict[str, Any]):
+    def fromDict(cls, data: dict[str, ValidInComponentValue]):
         "Requires a dictionary where the keys are the names of components and the values are their values."
-        components = {
+        allowedFields = {f.name for f in fields(cls)}
+
+        main_fields = {
             component.split(":")[-1]: value
             for component, value
             in data.items()
-            if not component.startswith("!")
+            if component.split("minecraft:")[-1] in allowedFields
+            and not component.startswith("!")
         }
-        components.update({
-            component.split(":")[-1]: REMOVED
-            for component
-            in data
-            if component.startswith("!")
+        main_fields.update({
+            component.split("!")[-1].split(":")[-1]: REMOVED
+            for component, value
+            in data.items()
+            if component.split("!")[-1].split(":")[-1] in allowedFields
+            and component.startswith("!")
         })
-        
-        return cls(**components)
-    
-    def toDict(self) -> dict:
-        _ = {
+        other_fields = {
             component: value
             for component, value
-            in asdict(self).items()
-            if value is not REMOVED and value is not None
+            in data.items()
+            if component.split("minecraft:")[-1] not in allowedFields
+            and not component.startswith("!")
         }
-        _.update({
-            "!" + component: {}
+        other_fields.update({
+            component.split("!")[-1]: REMOVED
             for component, value
-            in asdict(self).items()
+            in data.items()
+            if component.split("!")[-1].split("minecraft:")[-1] not in allowedFields
+            and component.startswith("!")
+        })
+        
+        return cls(**{
+            **{component: value for component, value in main_fields.items()},
+            "_other_components": other_fields
+        })
+
+    def asDict(self) -> dict[str, ValidInComponentValue]:
+        allowedFields = {f.name for f in fields(self)}
+
+        main_fields = {
+            f"minecraft:{field}": getattr(self, field)
+            for field in allowedFields
+            if getattr(self, field) is not None
+            and getattr(self, field) is not REMOVED
+            and field != "_other_components"
+        }
+        main_fields.update({
+            f"!minecraft:{field}": {}
+            for field in allowedFields
+            if getattr(self, field) is REMOVED
+            and field != "_other_components"
+        })
+        other_fields = {
+            key: value
+            for key, value in getattr(self, "_other_components", {}).items()
+            if value is not REMOVED
+        }
+        other_fields.update({
+            f"!{key}": None
+            for key, value in getattr(self, "_other_components", {}).items()
             if value is REMOVED
         })
 
-        return _
+        return {
+            **main_fields,
+            **other_fields
+        }
+    
+    def __getitem__(self, query: str) -> ValidComponentValue | None:
+        return (
+            getattr(self, query.split(":")[-1])
+            or self._other_components.get(query)
+            or None
+        )
 
-print(ItemComponents.fromVanillaItem("elytra").toDict())
+    def __setitem__(self, query: str, value: ValidComponentValue) -> None:
+        allowedFields = {f.name for f in fields(self)}
+        if query.split(":")[-1] in allowedFields:
+            setattr(self, query.split(":")[-1], value)
+        else:
+            self._other_components[query] = value
+
+components = ItemComponents.fromVanillaItem("elytra")
+print(components.asDict())
+print(components["equippable"])
+components["equippable"] = REMOVED
+print(components["equippable"])
